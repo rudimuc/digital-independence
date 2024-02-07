4eb6479d4bd0ed307305fedd0c37709a368180ab
rudi <commerce@haeuslernet.de>
2024-02-07 16:28:13 +0100

    reduced folders to include in dlna server

--- dlna/minidlna/start.sh
+++ dlna/minidlna/start.sh
@@ -31,29 +31,6 @@ if [[ ! -z "${ALL_MEDIA_DIR3}" ]]; then
   echo "media_dir=${ALL_MEDIA_DIR3}" >> "$TEMP_CONF"
 fi
 
-if [[ ! -z "${ALL_MEDIA_DIR4}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR4}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${ALL_MEDIA_DIR5}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR5}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${ALL_MEDIA_DIR6}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR6}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${ALL_MEDIA_DIR7}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR7}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${ALL_MEDIA_DIR8}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR8}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${ALL_MEDIA_DIR9}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR9}" >> "$TEMP_CONF"
-fi
 
 # Video
 if [[ ! -z "${VIDEO_DIR1}" ]]; then
@@ -68,29 +45,7 @@ if [[ ! -z "${VIDEO_DIR3}" ]]; then
   echo "media_dir=V,${VIDEO_DIR3}" >> "$TEMP_CONF"
 fi
 
-if [[ ! -z "${VIDEO_DIR4}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR4}" >> "$TEMP_CONF"
-fi
 
-if [[ ! -z "${VIDEO_DIR5}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR5}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${VIDEO_DIR6}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR6}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${VIDEO_DIR7}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR7}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${VIDEO_DIR8}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR8}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${VIDEO_DIR9}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR9}" >> "$TEMP_CONF"
-fi
 
 # Audio
 if [[ ! -z "${AUDIO_DIR1}" ]]; then
@@ -105,29 +60,7 @@ if [[ ! -z "${AUDIO_DIR3}" ]]; then
   echo "media_dir=A,${AUDIO_DIR3}" >> "$TEMP_CONF"
 fi
 
-if [[ ! -z "${AUDIO_DIR4}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR4}" >> "$TEMP_CONF"
-fi
 
-if [[ ! -z "${AUDIO_DIR5}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR5}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${AUDIO_DIR6}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR6}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${AUDIO_DIR7}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR7}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${AUDIO_DIR8}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR8}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${AUDIO_DIR9}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR9}" >> "$TEMP_CONF"
-fi
 
 # Pictures
 if [[ ! -z "${PICTURES_DIR1}" ]]; then
@@ -142,29 +75,7 @@ if [[ ! -z "${PICTURES_DIR3}" ]]; then
   echo "media_dir=P,${PICTURES_DIR3}" >> "$TEMP_CONF"
 fi
 
-if [[ ! -z "${PICTURES_DIR4}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR4}" >> "$TEMP_CONF"
-fi
 
-if [[ ! -z "${PICTURES_DIR5}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR5}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${PICTURES_DIR6}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR6}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${PICTURES_DIR7}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR7}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${PICTURES_DIR8}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR8}" >> "$TEMP_CONF"
-fi
-
-if [[ ! -z "${PICTURES_DIR9}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR9}" >> "$TEMP_CONF"
-fi
 
 
 ## Sometimes PID file can exist after restart. Cleaning.
